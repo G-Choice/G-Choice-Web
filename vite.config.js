c98de77259b212e48bc2c57265a7bc5f24d32011
nguyenthiutvien <ntuv003@gmail.com>
2024-03-27 11:33:59 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://be-gchoice.onrender.com',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace(/^\/api/, '')
